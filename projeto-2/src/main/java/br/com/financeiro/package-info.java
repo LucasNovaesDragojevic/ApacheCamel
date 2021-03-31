ba18ceea339bd5454e0238074b0644a6807e00fd
Lucas Novaes Dragojevic <lucas.dragojevic@gmail.com>
2021-03-31 18:53:13 -0300

    Removido projeto desnecessário

--- projeto-2/src/main/java/br/com/financeiro/package-info.java
+++
@@ -1,6 +0,0 @@
-@jakarta.xml.bind.annotation.XmlSchema (
-      namespace="http://financeiro.com.br/nota",
-      elementFormDefault=jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED
-)
-package br.com.financeiro;
-
